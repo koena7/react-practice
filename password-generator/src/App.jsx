10461d8463dcb52b2018242c031b2f16fb9a0e64
koena7 <koena2204@gmail.com>
2026-01-06 14:30:19 +0530

    Created random password generator using useCallback, useState, useEffect

---
+++ password-generator/src/App.jsx
@@ -0,0 +1,77 @@
+import { useCallback, useEffect, useState } from 'react'
+
+function App() {
+  // Let's set the required variables
+  const [length, setLength] = useState(8);
+  const [charactersAllowed, setCharactersAllowed] = useState(false);
+  const [numbersAllowed, setNumbersAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // now we will use useCallback hook, as we need to generate password on different value changes but we also need to use caching and memoization.
+  const passGenerator = useCallback(() => {
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if(charactersAllowed) chars += "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    if(numbersAllowed) chars += "0123456789";
+
+    let generatedPass = "";
+
+    for(let i=1;i<=length;i++) {
+      let randomIndex = Math.floor(Math.random()*chars.length+1);
+      generatedPass += chars.charAt(randomIndex);
+    }
+    setPassword(generatedPass);
+  }, [length, numbersAllowed, charactersAllowed, setPassword])
+
+  useEffect(() => {
+    passGenerator();
+  }, [length, numbersAllowed, charactersAllowed, passGenerator])
+
+  return (
+    <div className='w-full h-screen flex flex-col items-center justify-center py-10 px-20'>
+        <div className='h-auto w-full p-5 text-center text-xl text-black m-5 px-10 bg-pink-300 rounded-2xl'>
+          <div className='text-center text-3xl text-black mb-5'>Password generator</div>
+          <div className='flex flex-row m-2 justify-center items-center'>
+            <input 
+              type="text" 
+              readOnly 
+              placeholder='password' 
+              value={password} 
+              className="w-full outline-none bg-amber-50 rounded-l-full p-2 overflow-hidden"/>
+              <button className="outline-none bg-pink-600 py-2 px-5 rounded-r-full">Copy</button>
+          </div>
+          <div className='flex flex-row m-2 justify-center items-center my-4'>
+            <div className='p-2'>
+              <input
+                id='lengthInput'
+                type='range'
+                min='8'
+                max='20'
+                onChange={(e)=>setLength(e.target.value)}
+                className='m-1'/>
+              <label htmlFor='lengthInput' className='m-1'>Length: {length}</label>
+            </div>
+
+            <div className='p-2'>
+              <input
+                id='numbersAllowed'
+                type='checkbox'
+                onChange={(e) => setNumbersAllowed(e.target.checked)}
+                className='m-1'/>
+              <label htmlFor='numbersAllowed' className='m-1'>Numbers</label>
+            </div>
+
+            <div className='p-2'>
+              <input
+                id='charactersAllowed'
+                type='checkbox'
+                onChange={() => setCharactersAllowed((prev)=>!prev)}
+                className='m-1'/>
+              <label htmlFor='charactersAllowed' className='m-1'>Characters</label>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
